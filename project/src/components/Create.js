cb436b190d5b2e551b3e84150f468f8fd3c8a6f7
unknown <zim21den21@gmail.com>
2021-05-21 18:34:33 +0300

    update function reload

--- project/src/components/Create.js
+++ project/src/components/Create.js
@@ -1,59 +1,67 @@
-import React from 'react';
-import { useState } from "react";
-import env from '../env.json';
+import React from "react"
+import { useState } from "react"
+import env from "../env.json"
 
 function Create() {
-    const [url, setUrl] = useState('');
-    const [lineClass, setLineClass] = useState('hide'); // hide div
-    const [formClass, setFormClass] = useState(''); // hide div
+  const [url, setUrl] = useState("")
+  const [lineClass, setLineClass] = useState("hide") // hide div
+  const [formClass, setFormClass] = useState("") // hide div
 
-    const sendData = (obj) => {
-        setFormClass('hide');
-        setLineClass('');
-        fetch(env.urlBackend, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            body: JSON.stringify(obj)
-        })
-            .then(response => response.json())
-            .then(response => {
-                console.log(response);
-                if (response.result) {
-                    setUrl(env.url + '/' + response.url);
-                }
-            })
-    }
+  const windowReloade = () => {
+    window.location.reload()
+  }
 
-    const LoadDataFromForm = (event) => {
-        event.preventDefault();
-        let note = event.target.elements.note.value;
-        note = note.trim();
-        if (note === '') {
-            alert('заполните поле');  // доделать всплывающее окно 
-            return false;
+  const sendData = (obj) => {
+    setFormClass("hide")
+    setLineClass("")
+    fetch(env.urlBackend, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: JSON.stringify(obj),
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response)
+        if (response.result) {
+          setUrl(env.url + "/" + response.url)
         }
-        sendData({ "note": note });
+      })
+  }
+
+  const LoadDataFromForm = (event) => {
+    event.preventDefault()
+    let note = event.target.elements.note.value
+    note = note.trim()
+    if (note === "") {
+      alert("заполните поле") // доделать всплывающее окно
+      return false
     }
-    return (
-        <div className="container">
-            <section className="create">
-                <h2>Create</h2>
-                <div className="form-note">
-                    <form onSubmit={LoadDataFromForm} className={formClass}>
-                        <label htmlFor="note">Enter a Note</label>
-                        <textarea name="note" id="note"></textarea>
-                        <button type="submit">Create</button>
-                    </form>
-                    <div className={lineClass} >
-                        <div className="hiden-div">{url}</div>
-                        <div><button onClick={() => window.location.reload()} className="hided-btn">Create new Note</button></div>
-                    </div>
-                </div>
-            </section>
+    sendData({ note: note })
+  }
+  return (
+    <div className="container">
+      <section className="create">
+        <h2>Create</h2>
+        <div className="form-note">
+          <form onSubmit={LoadDataFromForm} className={formClass}>
+            <label htmlFor="note">Enter a Note</label>
+            <textarea name="note" id="note"></textarea>
+            <button type="submit">Create</button>
+          </form>
+          <div className={lineClass}>
+            <div className="hiden-div">{url}</div>
+            <div>
+              <button onClick={windowReloade} className="hided-btn">
+                Create new Note
+              </button>
+            </div>
+          </div>
         </div>
-    );
+      </section>
+    </div>
+  )
 }
 
-export default Create;
+export default Create
